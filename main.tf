72c5a6366c9c0cb93bf13c3e13754be1e554cbc5
Sean Boyer <seanABoyer@gmail.com>
2022-01-12 00:49:58 -0600

    Routes and stuffs

--- main.tf
+++ main.tf
@@ -17,31 +17,60 @@ data "aws_ami" "debian" {
 
 resource "random_uuid" "server_name" {}
 
+resource "aws_key_pair" "ssh_key" {
+  key_name = "pz_ssh_key"
+  public_key = var.public_ssh_key
+
+  tags = {
+    Name = "Project_Zomboid-${random_uuid.server_name.result}"
+    Game = "Project_Zomboid"
+  }
+}
+
+
+/*
+ Network Start
+*/ 
+resource "aws_route" "route_ign_to_vpc" {
+  route_table_id = aws_vpc.vpc.main_route_table_id
+  destination_cidr_block = var.cidr_block
+  gateway_id = aws_internet_gateway.gw.id
+  
+  tags = {
+    Name = "Project_Zomboid-${random_uuid.server_name.result}"
+    Game = "Project_Zomboid"
+  }
+}
 
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "Project_Zomboid-${random_uuid.server_name.result}"
-        Game = "Project_Zomboid"
-    }
+  cidr_block = var.cidr_block
+  tags = {
+      Name = "Project_Zomboid-${random_uuid.server_name.result}"
+      Game = "Project_Zomboid"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc.id
+
+  tags = {
+      Name = "Project_Zomboid-${random_uuid.server_name.result}"
+      Game = "Project_Zomboid"
+  }
 }
 
 resource "aws_subnet" "subnet" {
-    vpc_id = aws_vpc.vpc.id
-    cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
 
-     map_public_ip_on_launch = true
+    map_public_ip_on_launch = true
 
 
-     depends_on = [aws_internet_gateway.gw]
-    tags = {
-        Name = "Project_Zomboid-${random_uuid.server_name.result}"
-        Game = "Project_Zomboid"
-    }
+    depends_on = [aws_internet_gateway.gw]
+  tags = {
+      Name = "Project_Zomboid-${random_uuid.server_name.result}"
+      Game = "Project_Zomboid"
+  }
 }
 
 resource "aws_eip" "eip" {
@@ -49,7 +78,16 @@ resource "aws_eip" "eip" {
   vpc = true
   associate_with_private_ip = "10.0.1.100"
   depends_on                = [aws_internet_gateway.gw]
+
+  tags = {
+    Name = "Project_Zomboid-${random_uuid.server_name.result}"
+    Game = "Project_Zomboid"
+  }
 }
+
+/*
+ Network End
+*/
 
 resource "aws_instance" "pz_server" {
   ami           = data.aws_ami.debian.id
@@ -65,13 +103,38 @@ resource "aws_instance" "pz_server" {
   subnet_id = aws_subnet.subnet.id
 
   key_name = "pz_ssh_key"
-}
-resource "aws_key_pair" "ssh_key" {
-  key_name = "pz_ssh_key"
-  public_key = var.public_ssh_key
 
-    tags = {
-    Name = "Project_Zomboid-${random_uuid.server_name.result}"
-    Game = "Project_Zomboid"
+  security_groups = [aws_security_group.security_group]
+}
+
+resource "aws_security_group" "security_group" {
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+  egress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+  egress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+
+  tags = {
+      Name = "Project_Zomboid-${random_uuid.server_name.result}"
+      Game = "Project_Zomboid"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "public_ssh_key" {
   type = string
 }
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
