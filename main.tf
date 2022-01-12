1ada6ae331bbdf5eac5c41d686f886e8a2fdb845
Sean Boyer <seanABoyer@gmail.com>
2022-01-12 01:16:01 -0600

    1am typo 20 -> 22

--- main.tf
+++ main.tf
@@ -117,8 +117,8 @@ resource "aws_security_group" "security_group" {
   }
   ingress {
     description = "SSH"
-    from_port = 20
-    to_port = 20
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
