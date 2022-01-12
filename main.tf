f1e363818271766147faa5486cbbddb586ddabb5
Sean Boyer <seanABoyer@gmail.com>
2022-01-12 01:18:25 -0600

    remove 80/443 from access

--- main.tf
+++ main.tf
@@ -109,23 +109,9 @@ resource "aws_security_group" "security_group" {
   vpc_id = aws_vpc.vpc.id
 
   ingress {
-    description = "HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     description = "SSH"
     from_port = 22
     to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTPS"
-    from_port = 443
-    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -136,7 +122,6 @@ resource "aws_security_group" "security_group" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
       Name = "Project_Zomboid-${random_uuid.server_name.result}"
       Game = "Project_Zomboid"
