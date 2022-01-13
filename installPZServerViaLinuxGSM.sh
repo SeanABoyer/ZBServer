6b09dadc4c0baf3d531242e6312abbb8043d9b61
Sean Boyer <seanABoyer@gmail.com>
2022-01-13 00:48:58 -0600

    add non-free and contrib

--- installPZServerViaLinuxGSM.sh
+++ installPZServerViaLinuxGSM.sh
@@ -15,11 +15,17 @@ sudo apt-get update -y
 finishLog "Updating System"
 
 startLog "Installing Packages"
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install bc binutils jq lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 netcat openjdk-11-jre rng-tools steamcmd tmux unzip
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository contrib
+sudo apt-add-repository non-free
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install bc binutils jq lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 netcat openjdk-11-jre rng-tools steamcmd tmux unzip
 finishLog "Installing Packages"
 
 startLog "Creating User and Changing User"
 sudo useradd pzserver -p $password -m
+sudo chown -R pzserver:pzserver /home/pzserver
 sudo su pzserver -c "wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh pzserver && yes | ./pzserver install"
 finishLog "Creating User and Changing User"
 
