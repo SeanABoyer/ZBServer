7ed13c0605bdc78ca662855b57b93b553ce327ad
Sean Boyer <seanABoyer@gmail.com>
2022-01-12 23:56:46 -0600

    adduser to useradd

--- installPZServerViaLinuxGSM.sh
+++ installPZServerViaLinuxGSM.sh
@@ -1,20 +1,20 @@
 startLog () {
-    log_message = $1
+    log_message=$1
     date=$(date '+%d/%m/%Y %H:%M:%S')
     echo "[${date}][Starting] ${log_message}"
 }
 finishLog () {
-    log_message = $1
+    log_message=$1
     date=$(date '+%d/%m/%Y %H:%M:%S')
     echo "[${date}][Completed] ${log_message}"
 }
 
 startLog "Updating System"
-sudo apt-get update
+sudo apt-get update -y
 finishLog "Updating System"
 
 startLog "Creating User and Changing User"
-sudo adduser pzserver -p $1
+sudo useradd pzserver -p $1
 sudo su pzserver
 finishLog "Creating User and Changing User"
 
