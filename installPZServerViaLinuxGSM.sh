ea9cedc8bd388f33639c5d3dcc1142637f84522e
Sean Boyer <seanABoyer@gmail.com>
2022-01-13 00:11:38 -0600

    useradd issues

--- installPZServerViaLinuxGSM.sh
+++ installPZServerViaLinuxGSM.sh
@@ -1,20 +1,21 @@
+password=$1
 startLog () {
     log_message=$1
     date=$(date '+%d/%m/%Y %H:%M:%S')
-    echo "[${date}][Starting] ${log_message}"
+    echo "[$date][Starting] log_message"
 }
 finishLog () {
     log_message=$1
     date=$(date '+%d/%m/%Y %H:%M:%S')
-    echo "[${date}][Completed] ${log_message}"
+    echo "[$date][Completed] $log_message"
 }
-
+startLog "Password is $password"
 startLog "Updating System"
 sudo apt-get update -y
 finishLog "Updating System"
 
 startLog "Creating User and Changing User"
-sudo useradd pzserver -p $1
+sudo useradd pzserver -p $password -m
 sudo su pzserver
 finishLog "Creating User and Changing User"
 
